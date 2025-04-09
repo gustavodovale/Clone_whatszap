ab622e84739f658ea9a4c26f74839c3ac4e79e53
gustavo <gust.dovale@gmail.com>
2025-04-08 23:25:16 -0300

    adicionando novas fotos

--- lib/main.dart
+++ lib/main.dart
@@ -68,7 +68,7 @@ class Home extends StatelessWidget {
             child: ListTile(
               leading: CircleAvatar(
               radius: 25,
-              backgroundImage: NetworkImage('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoNwTfP0KVPfRBPIkaNWuOyGBmVN0TzlXMSA&s'),),
+              backgroundImage: NetworkImage('https://super.abril.com.br/wp-content/uploads/2018/05/humanidade-causa-cc3a2ncer-em-animais-selvagens.png?crop=1&resize=1212,909'),),
               title: Text('joão'),
               subtitle: Text('Mensagem recente...'),
               
@@ -79,7 +79,7 @@ class Home extends StatelessWidget {
             child: ListTile(
               leading: CircleAvatar(
               radius: 25,
-              backgroundImage: NetworkImage('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoNwTfP0KVPfRBPIkaNWuOyGBmVN0TzlXMSA&s'),),
+              backgroundImage: NetworkImage('https://conafer.org.br/wp-content/uploads/2024/10/image2-4.png'),),
               title: Text('silva'),
               subtitle: Text('Mensagem recente...'),
               
@@ -90,7 +90,7 @@ class Home extends StatelessWidget {
             child: ListTile(
               leading: CircleAvatar(
               radius: 25,
-              backgroundImage: NetworkImage('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoNwTfP0KVPfRBPIkaNWuOyGBmVN0TzlXMSA&s'),),
+              backgroundImage: NetworkImage('https://conafer.org.br/wp-content/uploads/2022/10/566f045d0e2163522f00f274thinkstockphotos-479667835-1-scaled-1.webp'),),
               title: Text('alberto'),
               subtitle: Text('Mensagem recente...'),
               
@@ -101,7 +101,7 @@ class Home extends StatelessWidget {
             child: ListTile(
               leading: CircleAvatar(
               radius: 25,
-              backgroundImage: NetworkImage('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoNwTfP0KVPfRBPIkaNWuOyGBmVN0TzlXMSA&s'),),
+              backgroundImage: NetworkImage('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbM5TWWCp84uthGMfP4-eIAXsHMTpQGLH4Jw&s'),),
               title: Text('sanderson'),
               subtitle: Text('Mensagem recente...'),
               
@@ -112,7 +112,7 @@ class Home extends StatelessWidget {
             child: ListTile(
               leading: CircleAvatar(
               radius: 25,
-              backgroundImage: NetworkImage('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoNwTfP0KVPfRBPIkaNWuOyGBmVN0TzlXMSA&s'),),
+              backgroundImage: NetworkImage('https://vegazeta.com.br/wp-content/uploads/2021/03/Arthurs-Acres-Animal-Sanctuary-2.jpg'),),
               title: Text('matheus'),
               subtitle: Text('Mensagem recente...'),
               
@@ -123,7 +123,7 @@ class Home extends StatelessWidget {
             child: ListTile(
               leading: CircleAvatar(
               radius: 25,
-              backgroundImage: NetworkImage('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoNwTfP0KVPfRBPIkaNWuOyGBmVN0TzlXMSA&s'),),
+              backgroundImage: NetworkImage('https://bonitour.com.br/wp-content/uploads/2021/07/Blog-Capa-Os-melhores-destinos-brasileiros-para-a-observacao-de-animais-selvagens-Bonitour.webp'),),
               title: Text('Familia'),
               subtitle: Text('Mensagem recente...'),
               
@@ -134,7 +134,7 @@ class Home extends StatelessWidget {
             child: ListTile(
               leading: CircleAvatar(
               radius: 25,
-              backgroundImage: NetworkImage('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoNwTfP0KVPfRBPIkaNWuOyGBmVN0TzlXMSA&s'),),
+              backgroundImage: NetworkImage('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTqkRekG8Yny5LdXqXMJBWH2T7KaLcZXJ2Uw&s'),),
               title: Text('Gustavo'),
               subtitle: Text('Mensagem recente...'),
               
@@ -145,7 +145,7 @@ class Home extends StatelessWidget {
             child: ListTile(
               leading: CircleAvatar(
               radius: 25,
-              backgroundImage: NetworkImage('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoNwTfP0KVPfRBPIkaNWuOyGBmVN0TzlXMSA&s'),),
+              backgroundImage: NetworkImage('https://cdn.acritica.net/img/pc/920/600/dn_arquivo/2023/03/unnamed_2.jpg'),),
               title: Text('Nicolas'),
               subtitle: Text('Mensagem recente...'),
               
@@ -156,7 +156,7 @@ class Home extends StatelessWidget {
             child: ListTile(
               leading: CircleAvatar(
               radius: 25,
-              backgroundImage: NetworkImage('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoNwTfP0KVPfRBPIkaNWuOyGBmVN0TzlXMSA&s'),),
+              backgroundImage: NetworkImage('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnzJh8YwW_KQFKtfXrSrMLLE-JtusaKR_8Kg&s'),),
               title: Text('Tropa dos 4'),
               subtitle: Text('Mensagem recente...'),
               
